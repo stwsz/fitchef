f8d17fb8b40cd1c4add9ef1f7fcfe9adf04c5001
Igor Gabriel <igorgabsprofissional@gmail.om>
2024-12-02 21:41:14 -0300

    Fixing the layout

--- pages/home-page/home-page.js
+++ pages/home-page/home-page.js
@@ -1,23 +1,59 @@
 // Opening a language menu
+let languageOpen = false;
 const languageClick = document.getElementById('language-click');
 const languageUl = document.getElementById('language-ul');
 
+languageUl.style.display = 'none';
+
 languageClick.addEventListener('click', () => {
-    if (languageUl.style.display === 'none' || languageUl.style.display === '') {
+    if ((languageUl.style.display === 'none' || languageUl.style.display === '') && !languageOpen) {
         languageUl.style.display = 'flex';
+        languageOpen = true;
+        
+        // Close the sign menu if it's open
+        if (signOpen) {
+            signUl.style.display = 'none';
+            signOpen = false;
+        }
     } else {
         languageUl.style.display = 'none';
+        languageOpen = false;
     }
 });
 
 // Opening a login/registration menu
+let signOpen = false;
 const signClick = document.getElementById('sign-click');
 const signUl = document.getElementById('sign-ul');
 
+signUl.style.display = 'none';
+
 signClick.addEventListener('click', () => {
-    if (signUl.style.display === 'none' || signUl.style.display === '') {
+    if ((signUl.style.display === 'none' || signUl.style.display === '') && !signOpen) {
         signUl.style.display = 'flex';
+        signOpen = true;
+
+        if (languageOpen) {
+            languageUl.style.display = 'none';
+            languageOpen = false;
+        }
     } else {
         signUl.style.display = 'none';
+        signOpen = false;
     }
 });
+
+fetch('./home-page-pt.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('First then');
+        }
+
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Second then', error);
+    });
